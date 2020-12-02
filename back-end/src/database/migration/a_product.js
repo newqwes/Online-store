317fbac2213223a6ddb7e294b1da3507aef20256
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2020-12-02 14:18:51 +0300

    refactor(database): delete photo table, now photo in product

--- back-end/src/database/migration/a_product.js
+++ back-end/src/database/migration/a_product.js
@@ -4,12 +4,15 @@ exports.up = function (knex) {
       table.increments('id').primary();
       table.string('name', 100).notNullable();
       table.string('description', 500).notNullable();
+      table.string('photo_url', 500).notNullable();
     })
     .then(() =>
       knex('products').insert([
         {
           name: 'Pizza Smile',
           description: 'The Best Pizza of world!',
+          photo_url:
+            'https://images.dominos.by/media/dominos/osg/api/2020/04/21/tsyplenok__dominator_small-min.png',
         },
       ])
     );
