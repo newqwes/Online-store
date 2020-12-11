ac869de2b55034a6f57b7340500cdaa63301c0ec
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2020-12-11 12:04:57 +0300

    feat(product): patch update and refactor

--- back-end/src/services/productsService.js
+++ back-end/src/services/productsService.js
@@ -3,7 +3,9 @@ import Product from '../database/models/product';
 import Option from '../database/models/option';
 
 class ProductsService {
-  createResult = (status, data = {}) => ({ status, message: 'Success', data });
+  createResult = (status, data = {}, message = 'Success') => ({ status, message, data });
+
+  createNotFound = (id) => ({ status: 404, message: 'No such item found', data: +id });
 
   async getAll() {
     try {
@@ -22,7 +24,9 @@ class ProductsService {
         include: { model: Option, as: 'options' },
       });
 
-      return this.createResult(200, product);
+      if (product) return this.createResult(200, product);
+
+      return this.createNotFound(id);
     } catch (error) {
       return errorHandler(error);
     }
@@ -30,7 +34,25 @@ class ProductsService {
 
   async update(id, body) {
     try {
-      const product = await Product.update(body, {
+      const { options, ...param } = body;
+
+      const optionsWithId = options.map((n) => {
+        const option = n;
+        option.product_id = +id;
+        return option;
+      });
+
+      const [isFound] = await Product.update(param, {
+        where: { id },
+      });
+
+      if (!isFound) return this.createNotFound(id);
+
+      await Option.destroy({ where: { product_id: id } });
+
+      await Option.bulkCreate(optionsWithId);
+
+      const product = await Product.findOne({
         where: { id },
         include: { model: Option, as: 'options' },
       });
@@ -43,9 +65,11 @@ class ProductsService {
 
   async delete(id) {
     try {
-      await Product.destroy({ where: { id } });
+      const isFound = await Product.destroy({ where: { id } });
 
-      return this.createResult(200);
+      if (isFound) return this.createResult(200);
+
+      return this.createNotFound(id);
     } catch (error) {
       return errorHandler(error);
     }
