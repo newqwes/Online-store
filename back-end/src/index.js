35e8e6c460da98ddc4a77ff7c3b9f71cccd2347a
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2020-12-15 15:54:34 +0300

    init(swagger): add swagger to project

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import passport from 'passport';
 import morgan from 'morgan';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 import productsRoute from './routes/productsRoute';
 import authRoute from './routes/authRoute';
 import mwPassport from './middleware/passport';
@@ -22,6 +24,8 @@ app.use('/api/auth', authRoute);
 app.use('/api/products', productsRoute);
 
 const port = process.env.SERVER_PORT;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, async () => {
   // eslint-disable-next-line no-console
