6379773a43b0c45a15e85fc146cdcae257576dc9
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2020-12-17 13:48:32 +0300

    refactor(style): logo style, height header

--- front-end/src/components/Logo/Logo.jsx
+++ front-end/src/components/Logo/Logo.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LogoWrapper from './styled';
 
-const Logo = (props) => (
-  <LogoWrapper width={props.width} height={props.height}>
-    <a href={props.href}></a>
+const Logo = ({ width, href }) => (
+  <LogoWrapper width={width}>
+    <a href={href}></a>
   </LogoWrapper>
 );
 
 Logo.propTypes = {
   width: PropTypes.number,
   href: PropTypes.string,
-  height: PropTypes.number,
 };
 
 Logo.defaultProps = {
   width: 150,
-  height: 70,
 };
 
 export default Logo;
--- front-end/src/components/Logo/styled.js
+++ front-end/src/components/Logo/styled.js
@@ -4,12 +4,9 @@ import logo from '../../assets/images/logo.png';
 const LogoWrapper = styled.div`
   background-image: url(${logo});
   width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
   background-size: contain;
   background-repeat: no-repeat;
-  a {
-    display: block;
-  }
+  margin-top: 1%;
 `;
 
 export default LogoWrapper;
