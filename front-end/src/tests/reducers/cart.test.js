f02cb97b282f89711c2cade6551f48d823c015d3
Nazar Ashirov <newnewqwes@gmail.com>
2021-02-10 14:13:25 +0300

    refactor: remove test

--- front-end/src/tests/reducers/cart.test.js
+++
@@ -1,7 +0,0 @@
-import cart from '../../reducers/cart';
-
-describe('Reducer cart', () => {
-  it('should return the empty initial state', () => {
-    expect(cart(undefined, {})).toEqual([]);
-  });
-});
