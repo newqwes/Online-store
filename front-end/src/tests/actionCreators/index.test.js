7a9d145347d140026d35ea5440f24fbeb2114e0a
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2021-02-11 15:45:22 +0300

    refactor: remove some mocks

--- front-end/src/tests/actionCreators/index.test.js
+++ front-end/src/tests/actionCreators/index.test.js
@@ -1,18 +1,10 @@
 import { addToCart } from '../../actionCreators';
 import { ADD_TO_CART } from '../../actions';
+import mockProductItem from '../helpers/mocks';
 
 describe('actions', () => {
   it('addToCart action creator', () => {
-    const payload = {
-      id: 0,
-      type: 'pizza',
-      name: 'Фермерская',
-      description: 'Описание небольшое',
-      photoUrl: 'http://s.ru',
-      unitSign: 'гр',
-      currencySign: 'р.',
-      option: { id: 12, price: 12.25, weight: 750, product_id: 0 },
-    };
+    const payload = mockProductItem;
 
     const expectedAction = {
       type: ADD_TO_CART,
