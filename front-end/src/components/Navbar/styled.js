843797caf7a12b10c50474c2c706d6a203bdec66
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2021-02-16 13:06:58 +0300

    Feature/sticky header and navbar

--- front-end/src/components/Navbar/styled.js
+++ front-end/src/components/Navbar/styled.js
@@ -1,28 +1,25 @@
 import styled from 'styled-components';
 
-import SECTION_WIDTH from '../../constants/sectionWidth';
-
 export const NavbarItems = styled.ul`
   display: flex;
-  flex-direction: ${(props) => (props.horizontally ? 'row' : 'column')};
-  justify-content: space-between;
+  flex-direction: ${({ horizontally }) => (horizontally ? 'row' : 'column')};
+  justify-content: space-around;
   width: 100%;
-  max-width: ${SECTION_WIDTH.midle}px;
-  margin: 0 auto;
-  padding: 12px 0;
+  padding: 10px 0;
+  background-color: ${({ theme, themeVariant }) => theme.navbar.background[themeVariant]};
 
   a {
     text-decoration: none;
     padding: 12px 16px;
     font-size: 16px;
     letter-spacing: 0.1rem;
-    color: ${(props) => props.theme.navbar.color[props.themeVariant]};
+    color: ${({ theme, themeVariant }) => theme.navbar.color[themeVariant]};
     transition: 0.2s ease;
-    font-weight: ${(props) => props.fontWeight};
+    font-weight: bold;
     position: relative;
 
     &:hover {
-      color: ${(props) => props.theme.link.hover[props.themeVariant]};
+      color: ${({ theme, themeVariant }) => theme.link.hover[themeVariant]};
 
       &::after {
         width: 100%;
@@ -30,7 +27,7 @@ export const NavbarItems = styled.ul`
     }
 
     &:focus {
-      color: ${(props) => props.theme.link.focus[props.themeVariant]};
+      color: ${({ theme, themeVariant }) => theme.link.focus[themeVariant]};
 
       &::after {
         width: 100%;
@@ -43,15 +40,18 @@ export const NavbarItems = styled.ul`
       position: absolute;
       width: 0;
       left: 0;
-      bottom: 0;
-      border-radius: 15px 15px 0 0;
-      height: 8px;
-      background-color: ${(props) => props.theme.navbar.hoverBackground[props.themeVariant]};
-      transition: 0.5s ease;
+      bottom: 2px;
+      height: 4px;
+      background-color: ${({ theme, themeVariant }) => theme.link.line[themeVariant]};
+      transition: 0.2s ease;
     }
   }
 `;
 
 export const NavbarWrapper = styled.nav`
-  background-color: ${(props) => props.theme.navbar.background[props.themeVariant]};
+  position: sticky;
+  display: flex;
+  top: 60px;
+  justify-content: center;
+  box-shadow: 0 4px 10px ${({ theme, themeVariant }) => theme.navbar.boxShadow[themeVariant]};
 `;
