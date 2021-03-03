18c887fd5b920b18cab845f4b1762db1679a457d
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2021-03-03 13:27:30 +0300

    refactor: rename api utils file

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-import { extractResponsData, getToken } from '../utils/apiUtils';
+import { extractResponsData, getToken } from '../utils/api';
 
 const instance = Axios.create({
   baseURL: 'http://localhost:3005/api/',
